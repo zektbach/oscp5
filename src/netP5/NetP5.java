ff4a6436c9a0557cd0e615e4fd33b124d850592e
sojamo <sojamo@88dc9258-2ef1-11de-874f-bb9810f3bc79>
2010-01-23 16:29:41 +0000

    first commit, starting oscP5 at version 0.9.5

---
+++ src/netP5/NetP5.java
@@ -0,0 +1,29 @@
+package netP5;
+
+public interface NetP5 {
+  String VERSION = "0.9.4";
+  
+  boolean DEBUG = true;
+  
+  /**
+   * @related setNetworkProtocol ( )
+   */
+  public final static int UDP = 0;
+
+  /**
+   * @related setNetworkProtocol ( )
+   */
+  public final static int MULTICAST = 1;
+
+
+  /**
+   * @related setNetworkProtocol ( )
+   */
+  public final static int TCP = 2;
+  /**
+   * TODO
+   * authentification in AbstractTcpServer and  AbstractUdpServer. 
+   * TcpServer.authentificationRequired(true/false); 
+   * UdpServer.authentificationRequired(true/false);
+   */
+}
